991980c048520e0196f2b94be6b9453b654e1bd9
dominicod <dominicodonnell99@gmail.com>
2023-03-04 17:05:06 -0600

    Add SQLite DB

---
+++ BlogWebApp/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogWebApp.Data;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite(Configuration.GetConnectionString("BlogWebAppDatabase"));
+    }
+}
